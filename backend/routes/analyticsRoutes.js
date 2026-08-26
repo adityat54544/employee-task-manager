1a461d4f210a8db64bd63b035869430f930def8a
Aditya <aditya@portfolio.dev>
2026-08-26 17:25:04 +0530

    clean updated

--- backend/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-﻿const express = require("express");
-const router = express.Router();
-const { getDashboardStats } = require("../controllers/analyticsController");
-const { authenticate } = require("../middleware/authMiddleware");
-
-router.use(authenticate);
-router.get("/dashboard", getDashboardStats);
-
-module.exports = router;
